da206ee97ff51a2e0dc4ba849352cfcb66632727
Thomas Urban <thomas.urban@cepharum.de>
2020-05-01 14:20:55 +0200

    fixing unit tests on proper use of new server-dev-tools

--- test/unit/integration/api.js
+++ test/unit/integration/api.js
@@ -29,33 +29,28 @@
 const Path = require( "path" );
 
 const { describe, it, before, after } = require( "mocha" );
-const { start, stop, query: { get: GET } } = require( "hitchy-server-dev-tools" );
+const HitchyDev = require( "hitchy-server-dev-tools" );
 
 require( "should" );
 require( "should-http" );
 
 
 describe( "When integrating with Hitchy a controller", () => {
-	let server;
+	const ctx = {};
 
-	before( "starting hitchy server", () => {
-		return start( {
-			pluginsFolder: Path.resolve( __dirname, "../../.." ),
-			testProjectFolder: Path.resolve( __dirname, "../../project" ),
-			options: {
-				// debug: true,
-			},
-		} )
-			.then( instance => {
-				server = instance;
-			} );
-	} );
+	before( HitchyDev.before( ctx, {
+		pluginsFolder: Path.resolve( __dirname, "../../.." ),
+		testProjectFolder: Path.resolve( __dirname, "../../project" ),
+		options: {
+			// debug: true,
+		},
+	} ) );
 
-	after( "stopping hitchy server", () => stop( server ) );
+	after( HitchyDev.after( ctx ) );
 
 
 	it( "can access implicitly discovered models", () => {
-		return GET( "/models" )
+		return ctx.get( "/models" )
 			.then( res => {
 				res.should.have.status( 200 ).and.be.json();
 				res.data.should.be.an.Array().which.is.deepEqual( ["BasicData"] );
@@ -63,7 +58,7 @@ describe( "When integrating with Hitchy a controller", () => {
 	} );
 
 	it( "can access abstract model as a service component", () => {
-		return GET( "/modelClass" )
+		return ctx.get( "/modelClass" )
 			.then( res => {
 				res.should.have.status( 200 ).and.be.json();
 				res.data.should.be.true();
@@ -71,7 +66,7 @@ describe( "When integrating with Hitchy a controller", () => {
 	} );
 
 	it( "can access instance of implicitly discovered model which is having access on Hitchy's API", () => {
-		return GET( "/modelImplicitInstance" )
+		return ctx.get( "/modelImplicitInstance" )
 			.then( res => {
 				res.should.have.status( 200 ).and.be.json();
 				res.data.should.be.Object().which.has.property( "someString" ).which.is.equal( "BasicData" );
@@ -79,7 +74,7 @@ describe( "When integrating with Hitchy a controller", () => {
 	} );
 
 	it( "can define new model using Model service exposed by hitchy-plugin-odem and access instance of it which is having access on Hitchy's API", () => {
-		return GET( "/modelExplicitInstance" )
+		return ctx.get( "/modelExplicitInstance" )
 			.then( res => {
 				res.should.have.status( 200 ).and.be.json();
 				res.data.should.be.Object().which.has.property( "someString" ).which.is.equal( "BasicData" );
@@ -87,7 +82,7 @@ describe( "When integrating with Hitchy a controller", () => {
 	} );
 
 	it( "can define new model using Model exported by hitchy-plugin-odem and access instance of it which is having access on Hitchy's API", () => {
-		return GET( "/modelExplicitInstance" )
+		return ctx.get( "/modelExplicitInstance" )
 			.then( res => {
 				res.should.have.status( 200 ).and.be.json();
 				res.data.should.be.Object().which.has.property( "someString" ).which.is.equal( "BasicData" );
